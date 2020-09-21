3c3cf73dd652e3620a19cdd2327e410cc15e093e
amit-dhakad <amit0502dhakad@gmail.com>
2020-09-21 22:49:36 +0530

    added user controller and route

---
+++ app/url_mapping.go
@@ -0,0 +1,15 @@
+package app
+
+import (
+	"github.com/amit-dhakad/bookstore_user-api/controllers/ping"
+	"github.com/amit-dhakad/bookstore_user-api/controllers/users"
+)
+
+func mapUrls() {
+
+	router.GET("/ping", ping.Ping)
+
+	router.GET("/users/:user_id", users.GetUser)
+
+	router.POST("/users", users.CreateUser)
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/amit-dhakad/bookstore_user-api/app"
+)
+
+func main() {
+	app.StartApplication()
+}
