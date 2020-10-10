778e84cb9b7c853d298ab7651f4cd13b8d5b894f
Amit Dhakad <amit0502dhakad@gmail.com>
2020-10-10 08:33:27 -0700

    added db credential to environment variable

--- datasources/mysql/usersdb/user_db.go
+++ datasources/mysql/usersdb/user_db.go
@@ -4,17 +4,30 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql" // mysql driver
+)
+
+const (
+	mysqlUsersUsername = "mysqlUsersUsername"
+	mysqlUsersPassword = "mysqlUsersPassword"
+	mysqlUsersHost     = "mysqlUsersHost"
+	mysqlUserSchema    = "mysqlUserSchema"
 )
 
 // Client return db connection object
 var (
 	Client *sql.DB
+
+	username = os.Getenv(mysqlUsersUsername)
+	password = os.Getenv(mysqlUsersPassword)
+	host     = os.Getenv(mysqlUsersHost)
+	schema   = os.Getenv(mysqlUserSchema)
 )
 
 func init() {
-	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?character=utf8", "root", "toor", "127.0.0.1", "users_db")
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s", username, password, host, schema)
 	Client, err := sql.Open("mysql", dataSourceName)
 
 	if err != nil {
